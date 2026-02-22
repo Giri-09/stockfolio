5f7bc5c35e2428e79f8f2e4fe08eb3bbfb97abf5
Sai Giri <nsgiri4444@gmail.com>
2026-02-22 13:22:11 +0530

    API end point changes

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = process.env.NEXT_PUBLIC_API_URL || "/api";
 
 export async function fetchPortfolio() {
-  const { data } = await axios.get(`${API}/api/portfolio`);
+  const { data } = await axios.get(`${API}/portfolio`);
   return data;
 }
 
